904207e06d8d11be1026d893c17e275a6b0bb14a
iqbal61 <iqbalnafi679@gmail.com>
2022-11-18 15:29:08 +0700

    first commit

---
+++ scrip.js
@@ -0,0 +1,20 @@
+let lastImage = document.getElementsByClassName("last-img") [0];
+let MidImage = document.getElementsByClassName("mid-img")[0];
+let BirdImage = document.getElementsByClassName("bird-img")[0];
+let FocusImage = document.getElementsByClassName("focus-img")[0];
+let Leaf = document.getElementsByClassName("leaf-img")[0];
+let Traveling = document.querySelector("#landing h1");
+let Button = document.querySelector(".btn");
+
+window.addEventListener('scroll', function() {
+    let value = window.scrollY
+    BirdImage.style.left =value * 0.9 + 'px';
+    BirdImage.style.top = value * 0.7 + 'px';
+    lastImage.style.top = value * 0.7 + 'px';
+    MidImage.style.top = value * 0.3 + 'px';
+
+    FocusImage.style.top = value * 0 + 'px';
+    Leaf.style.left = value * 1 + 'px';
+    Traveling.style.right = value * 0.5 + 'px';
+    Button.style.marginTop = value * 0.1 + 'px';
+})
